2ac12f70699a9f917d96cabd3b9c948339605ada
KanaeTakahashi <lak.takahashi@gmail.com>
2020-05-08 15:59:22 +0900

    » #02 ToDoを追加していこう

--- myscript.js
+++ myscript.js
@@ -8,4 +8,19 @@ angular.module('myapp', [])
             {"body":"do this 3", "done":true},
             {"body":"do this 4", "done":false},
         ];
+
+        // 要素追加イベント
+        $scope.addNew = function(){
+            // tasks配列に要素を追加する
+            $scope.tasks.push({"body":$scope.newTaskBody, "done":false});
+            // 初期化
+            $scope.newTaskBody = '';
+        };
+
+        // 完了タスクを取得イベント
+        $scope.getDoneCount = function() {
+            var count = 0;
+            
+            return count;
+        }
     }]);
