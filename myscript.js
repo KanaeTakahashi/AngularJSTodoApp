8c7acb5c036c89e6e5d0908bc146b51c5cc27179
KanaeTakahashi <lak.takahashi@gmail.com>
2020-05-08 15:32:49 +0900

    » #01 ToDoを表示させよう

--- myscript.js
+++ myscript.js
@@ -2,5 +2,10 @@
 angular.module('myapp', [])
     // MainControllerを追加
     .controller('MainController', ['$scope', function($scope){
-
+        $scope.tasks = [
+            {"body":"do this 1", "done":false},
+            {"body":"do this 3", "done":false},
+            {"body":"do this 3", "done":true},
+            {"body":"do this 4", "done":false},
+        ];
     }]);
